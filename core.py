a43cb6b3bdcdd51a7eb184397d8605a73f5e1656
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-05-31 16:18:16 +0100

    Add shelve for key-value storage

--- core.py
+++ core.py
@@ -1,3 +1,6 @@
+import re
+import shelve
+
 import requests
 import yaml
 from bs4 import BeautifulSoup
@@ -9,6 +12,10 @@ import settings
 class Product:
     def __init__(self, product_url: str):
         self.url = product_url
+        if m := re.search(r'\d+$', self.url):
+            self.id = m.group()
+        else:
+            raise ValueError(f'Product url "{self.url}" does not include an id')
 
         response = requests.get(product_url)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -67,15 +74,62 @@ class User:
     def __init__(self, name: str, email: str):
         self.name = name
         self.email = email
-        self.sg = SendGrid(
-            settings.SENDGRID_APIKEY,
-            settings.NOTIFICATION_FROM_ADDR,
-            settings.NOTIFICATION_FROM_NAME,
+
+    def __str__(self):
+        return self.name
+
+
+class Tracking:
+    sg = SendGrid(
+        settings.SENDGRID_APIKEY,
+        settings.NOTIFICATION_FROM_ADDR,
+        settings.NOTIFICATION_FROM_NAME,
+    )
+    deliveries = shelve.open(settings.STORAGE_PATH)
+
+    def __init__(self, user: User, product: Product):
+        self.user = user
+        self.product = product
+
+    @property
+    def tagline(self):
+        return f'{self.user.email}:{self.product.id}'
+
+    def load(self) -> int | None:
+        return self.deliveries.get(self.tagline)
+
+    def save(self) -> None:
+        self.deliveries[self.tagline] = self.product.offer_price
+
+    def remove(self) -> None:
+        del self.deliveries[self.tagline]
+
+    def notify(self):
+        self.sg.send(
+            to=self.user.email,
+            subject=self.product.title,
+            msg=str(self.product),
+            as_markdown=True,
         )
 
-    def handle_product(self, product: Product):
-        if product.is_offer:
-            self.sg.send(to=self.email, subject=product.title, msg=str(product), as_markdown=True)
+    def dispatch(self):
+        notify = False
+        if self.product.is_offer:
+            if notified_offer_price := self.load():
+                # notified in the past
+                if self.product.offer_price < notified_offer_price:
+                    notify = True
+            else:
+                notify = True
+            if notify:
+                self.save()
+                self.notify()
+        elif self.notified:
+            self.remove()
+
+    @property
+    def notified(self):
+        return self.tagline in self.deliveries
 
 
 class IKEAOffers:
@@ -87,4 +141,5 @@ class IKEAOffers:
             user = User(user_cfg['name'], user_cfg['email'])
             for product_url in user_cfg['track']:
                 product = Product(product_url)
-                user.handle_product(product)
+                tracking = Tracking(user, product)
+                tracking.dispatch()
