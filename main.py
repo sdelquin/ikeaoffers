00140acec5b4989ab150b3fd14e237e0ef7db224
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-06-09 12:54:40 +0100

    Add typer for CLI management

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+import logzero
+import typer
+
 from core import IKEAOffers
 from utils import init_logger
 
+app = typer.Typer(add_completion=False)
 logger = init_logger()
 
 
-app = IKEAOffers()
-app.run()
+@app.command()
+def run(
+    loglevel: str = typer.Option(
+        'DEBUG', '--loglevel', '-l', help='Log level (debug, info, error)'
+    ),
+):
+    '''
+    Check for IKEA offers and notify users.
+    '''
+
+    logger.setLevel(getattr(logzero, loglevel.upper()))
+    offers = IKEAOffers()
+    offers.run()
+
+
+if __name__ == "__main__":
+    app()
