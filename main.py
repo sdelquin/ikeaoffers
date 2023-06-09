e19f6d133b1ee41125e3a3dd3c957bc2c4d4192c
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-06-09 14:46:09 +0100

    Add clear command

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import glob
+import os
+
 import logzero
 import typer
 
+import settings
 from core import IKEAOffers
 from utils import init_logger
 
@@ -14,13 +18,19 @@ def run(
         'DEBUG', '--loglevel', '-l', help='Log level (debug, info, error)'
     ),
 ):
-    '''
-    Check for IKEA offers and notify users.
-    '''
+    '''Check for IKEA offers and notify users.'''
 
     logger.setLevel(getattr(logzero, loglevel.upper()))
     offers = IKEAOffers()
     offers.run()
+
+
+@app.command()
+def clear():
+    '''Clear tracking database.'''
+    if typer.confirm('Are you sure to delete tracking database?'):
+        for file_path in glob.glob(settings.STORAGE_PATH + '*'):
+            os.remove(file_path)
 
 
 if __name__ == "__main__":
